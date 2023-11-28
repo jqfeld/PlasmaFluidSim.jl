6cf3c6fd769ad9234943ec6a0619b0ea5d68390c
Documenter.jl <documenter@juliadocs.github.io>
2023-11-28 12:11:51 +0000

    build based on 330dba0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlasmaFluidSim","category":"page"},{"location":"#PlasmaFluidSim","page":"Home","title":"PlasmaFluidSim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlasmaFluidSim.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlasmaFluidSim]","category":"page"}]
+}
